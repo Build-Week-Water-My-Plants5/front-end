e33f420ddb35524903d0dc1bdc7dc5ce849c3169
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2021-11-16 15:25:01 -0700

    working on login and signup components

--- water-my-plants/src/components/Login.js
+++ water-my-plants/src/components/Login.js
@@ -1,12 +1,60 @@
+import React from 'react';
+
+const Login = (props) => {
+
+    const { change, submit, disabled, errors } = props;
+    const { username, password } = props.value;
+
+    const onChange = evt => {
+        const { name, value, checked, type } = evt.target
+        const newValue = type === 'checkbox' ? checked : value;
+        change(name, newValue)
+    }
+    // dont think newValue is correct but wasnt sure what to put in place.
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+        submit()
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+
+            <div className='header'>
+                Login
+            </div>
+            <div className='box'>
+                <label>Username
+                    <input
+                        type='text'
+                        name='username'
+                        value={username}
+                        onChange={onChange}
+                    />
+                </label>
+                {errors.username}
+            </div>
 
 
-const Login = () => {
+            <div className='input-p'>
+                <label >Password
+                    <input
+                        type='text'
+                        name='password'
+                        value={password}
+                        onChange={onChange}
+                    />
+                </label>
+                {errors.password}
+            </div>
+            <button disabled={disabled}>Login</button>
 
-    return(
-        <div>
-
-        </div>
-    )
+        </form>
+    );
 }
+
+
+
+
 
 export default Login;
--- water-my-plants/src/components/SignUp.js
+++ water-my-plants/src/components/SignUp.js
@@ -1,12 +1,94 @@
 
 
 const SignUp = () => {
+    const { change, submit, disabled, errors } = props;
+    const { username, email, password, number } = props.value;
 
-    return(
-        <div>
+    const onChange = evt => {
+        const { name, value, checked, type } = evt.target
+        const newValue = type === 'checkbox' ? checked : value;
+        change(name, newValue)
+    }
 
-        </div>
-    )
+    const onSubmit = evt => {
+        evt.preventDefault()
+        submit()
+
+
+    }
+
+    return (
+
+        <form onSubmit={onSubmit}>
+            <div className='header'>
+                Sing SignUp
+            </div>
+        
+          {errors.password}
+          {errors.email}
+          {errors.number}
+            
+            
+            <div className='box'>
+                <label>Username
+                    <input
+                        type='text'
+                        name='username'
+                        value={username}
+                        onChange={onChange}
+                    />
+                </label>
+            </div>
+
+            <div>
+                <label>email
+                    <input
+                        type='text'
+                        name='email'
+                        value={email}
+                        onChange={onChange}
+                    />
+                </label>
+                {errors.username}
+            </div>
+
+            <div>
+                <label >Password
+                    <input
+                        type='text'
+                        name='password'
+                        value={password}
+                        onChange={onChange}
+                    />
+                </label>
+            </div>
+
+            <div>
+                <label >Phone Number
+                    <input
+                        type='text'
+                        name='number'
+                        value={number}
+                        onChange={onChange}
+                    />
+                </label>
+            </div>
+
+            <div>
+                <label> Terms of Service
+                    <input
+                        type='checkbox'
+                        name='tos'
+                        checked={tos}
+                        onChange={onChange}
+                    />
+                </label>
+            </div>
+            <button disabled={disabled}>Sign Up </button>
+
+        </form>
+    );
+
 }
 
 export default SignUp;
