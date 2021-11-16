81cf8912451ed18c605e6a5a99b6d6f99793318d
Dillon Maks <dillon@Dillons-MacBook-Pro.local>
2021-11-16 12:43:50 -0600

    Added Home & SignOut Components.

--- water-my-plants/src/App.js
+++ water-my-plants/src/App.js
@@ -1,9 +1,36 @@
+import { Switch, Link } from 'react-router-dom';
 import './App.css';
+
+// Component Imports
+import PlantList from './components/PlantList';
+import Account from './components/Account';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <h1>TIME TO CODE!</h1>
+      <header>
+        <nav>
+          {/* Check for user auth, then render plantList Link */}
+          {/* Check for user auth, then render Account Link */}
+          {/* !user ? SignUp : SignOut */}
+        </nav>
+      </header>
+
+      <Switch>
+        <Route path='/signup'>
+          <SignUp />
+        </Route>
+        <Route path='/plants'>
+          <PlantList />
+        </Route>
+        <Route path='/account'>
+          <Account />
+        </Route>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
