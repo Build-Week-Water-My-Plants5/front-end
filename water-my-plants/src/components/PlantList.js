fe00edff5f677bc86cfdc83e5a1c0dc1b8059f7e
Dillon Maks <dillon@Dillons-MacBook-Pro.local>
2021-11-17 17:32:36 -0600

    Resolving issues with merge from PlantList

--- water-my-plants/src/components/PlantList.js
+++ water-my-plants/src/components/PlantList.js
@@ -1,64 +1,9 @@
-import { useState } from 'react';
-import Plant from './Plant';
+import React from 'react'
 
-
-const initFormVal = {
-    nickname: '',
-    image: '',
-    h20frequency: '',
-    species: ''
-}
-const PlantList = () => {
-    const [isEditing, setIsEditing] = useState(false);
-    const [formVal, setFormVal] = useState(initFormVal);
-    const [plants, setPlants] = useState([]);
-
-    const handleChange = (e) => {
-        setFormVal({
-            ...formVal,
-            [e.target.name]: e.target.value
-        })
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(formVal);
-    }
-
-    return(
+export default function PlantList() {
+    return (
         <div>
-            {isEditing && (
-                <form>
-                    <label>Nickname</label>
-                    <input type='text' name='nickname' onChange={handleChange} value={formVal.nickname}/>
-
-                    <label>Species</label>
-                    <input type='text' name='species' onChange={handleChange} value={formVal.species}/>
-
-                    <label>h20frequency</label>
-                    <input type='text' name='h20frequency' onChange={handleChange} value={formVal.h20frequency}/>
-
-                    <label>Image</label>
-                    <input type='text' name='image' onChange={handleChange} value={formVal.h20frequency}/>
-
-                    <button onClick={handleSubmit}>Save</button>
-                </form>)
-            }
-
-            {plants.map(item => <Plant data={item} setIsEditing={setIsEditing} isEditing={isEditing} setFormVal={setFormVal}/>)}
+            
         </div>
-    )
-}
-
-export default PlantList;
-
-const EditForm = () => {
-
-
-
-    return(
-        <form>
-
-        </form>
     )
 }
