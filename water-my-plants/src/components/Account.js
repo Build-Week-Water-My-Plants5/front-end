54003c79ed2fbdfc24003fdc261f7ca8d0f364ed
Dillon Maks <dillon@Dillons-MacBook-Pro.local>
2021-11-17 20:08:15 -0600

    Commits

--- water-my-plants/src/components/Account.js
+++ water-my-plants/src/components/Account.js
@@ -3,18 +3,23 @@ import axios from 'axios';
 
 
 const Account = () => {
+    const [user, setUser] = useState({});
+    const uid = localStorage.getItem('uid');
 
     useEffect(() => {
-        axios.get('https://water-the-plants-api.herokuapp.com/api/users/')
+        axios.get(`https://water-the-plants-api.herokuapp.com/api/users/${uid}`)
         .then(res => {
-            console.log(res);
+            setUser(res.data);
+            console.log(res.data);
         })
         .catch(err => console.log(err));
     }, [])
 
     return(
         <div>
-
+            <span>Username: {user.username}</span>
+            <span>{user.phone_number && user.phone_number}</span>
+            <span>ID: {user.user_id}</span>
         </div>
     )
 }
