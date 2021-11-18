91b935d3b8d965da42a04fa297d1eabb390af279
Dillon Maks <dillon@Dillons-MacBook-Pro.local>
2021-11-17 20:17:29 -0600

    Made fake data to use

---
+++ water-my-plants/src/data.js
@@ -0,0 +1,46 @@
+
+export const plants = [
+    {
+        id: 1,
+        nickname: 'Venus Fly Trap',
+        species: 'Dionaea muscipula',
+        h20Frequency: 8,
+    },
+    {
+        id: 2,
+        nickname: 'Common Sunflower',
+        species: 'Helianthus annuus',
+        h20Frequency: 3,
+    },
+    {
+        id: 3,
+        nickname: 'Poppy',
+        species: 'Papaver somniferum',
+        h20Frequency: 5,
+    },
+    {
+        id: 4,
+        nickname: 'Hibiscus',
+        species: 'Hibiscus rosa-sinensis',
+        h20Frequency: 6,
+    },
+    {
+        id: 5,
+        nickname: 'Rose',
+        species: 'Rosa',
+        h20Frequency: 7,
+    },
+    {
+        id: 6,
+        nickname: 'Shy Plant',
+        species: 'Mimosa pudica',
+        h20Frequency: 10,
+    }
+]
+
+export const mockUser = {
+    id: 1,
+    password: 'password',
+    username: 'mockdata',
+    phone: '999-999-9999'
+}
