b0025c0e6e0e6d4061ef9b22821a236ae1f82d0a
Brandyn <stateofw@gmail.com>
2021-11-17 22:09:23 -0500

    styled login and signup

--- water-my-plants/src/components/SignUp.js
+++ water-my-plants/src/components/SignUp.js
@@ -1,6 +1,37 @@
 import axios from 'axios';
 import { useHistory } from 'react-router';
 import React, { useState } from 'react';
+import styled from "styled-components";
+import '../SignUp.css'
+
+document.body.style.backgroundColor = "#b2c2c0";
+
+const StyledButton = styled.button`
+  background-color: #D2DECE;
+  font-size: 20px;
+  color: black;
+  border-radius: 5px;
+  width: 25%;
+  margin-bottom: 15px;
+`;
+
+const StyledH1 = styled.h1`
+  font-size: 32px;
+`;
+
+const StyledLabel = styled.label`
+    font-size: 20px;
+    font-weight: bold;
+    
+`;
+
+const StyledInput = styled.input`
+  width: 40%;
+  margin: auto;
+  display: block;
+`;
+
+
 
 
 const initFormVal = {
@@ -43,52 +74,56 @@ const SignUp = (props) => {
     }
 
     return (
-
-        <form onSubmit={onSubmit}>
+        <div className="container">
             <div className='header'>
-                Sign Up
+                <StyledH1>Sign Up</StyledH1>
             </div>
             
 
-            <div className='box'>
-                <label>Username
-                    <input
+            <div className="border">
+            <form onSubmit={onSubmit}>
+            <div className="box">
+                <StyledLabel>Username
+                    <StyledInput
                         type='text'
                         name='username'
                         value={formVal.username}
                         onChange={onChange}
                     />
-                </label>
+                </StyledLabel>
                 {errors.username}
             </div>
 
             <div>
-                <label >Password
-                    <input
+                <StyledLabel >Password
+                    <StyledInput
                         type='text'
                         name='password'
                         value={formVal.password}
                         onChange={onChange}
                     />
-                </label>
+                </StyledLabel>
                 {errors.password}
             </div>
 
             <div>
-                <label >Phone Number
-                    <input
+                <StyledLabel >Phone Number
+                    <StyledInput
                         type='text'
                         name='phoneNumber'
                         value={formVal.phoneNumber}
                         onChange={onChange}
                     />
-                </label>
+                </StyledLabel>
                 {errors.phoneNumber}
             </div>
 
-            <button disabled={disabled}>Sign Up </button>
+            <StyledButton disabled={disabled}>Sign Up </StyledButton>
+
 
         </form>
+        </div>
+        </div>
     );
 
 }
